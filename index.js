96bcd3fabcc207d97bd4a55eb7ca483570d69656
Ian Frankel <ian.frankel@gmail.com>
2023-10-06 12:35:58 -0400

    name tests added

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching (nameList, targetName) {
+    return nameList.filter(nom => equalName(nom, targetName))
+}
+
+function equalName(a,b) {
+    console.log(a.toLowerCase() === b.toLowerCase())
+    return a.toLowerCase() === b.toLowerCase()
+}
+
+function fuzzyMatch(nameList, targetStart) {
+    return nameList.filter(nom => nom.startsWith(targetStart))
+}
+
+function matchName(nameList, targetName) {
+    return nameList.filter(driver => driver.name === targetName)
+}
